ef0196c9e59553e804d55d04cccc2c0fa40c969e
Zhang Lihua <zhanglihua@wanchain.org>
2021-10-21 00:45:25 +0800

    update helper

---
+++ contracts/Helper.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.2;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+struct Order {
+  address user;
+  address fromToken;
+  address toToken;
+  uint    price;
+  uint    amount;
+  bool    isForever;
+  uint    interval; // seconds
+  uint    lastCheck; // only forever order
+  uint    received;
+}
+interface IIUniswapV2Router02{
+  function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+}
+interface ISwap{
+  function records(bytes32 key) external returns (Order memory record);
+  function router() external returns (address _router);
+  function swap(bytes32 key, address[] calldata path, uint256 _amountIn, uint256 _amountOut, bool isReflect, bool isPaid) external;
+}
+interface IEERC20 is IERC20 {
+  function symbol() external view returns (string memory);
+  function decimals() external view returns (uint8);
+}
+contract Helper is AccessControl {
+  using SafeMath for uint;
+  using SafeERC20 for IEERC20;
+
+  address public  mToken;
+  address public  swap;
+  address public  feeTo; 
+  address public  operator;
+  uint    public  discount;
+
+  constructor(address _admin, address _mToken, address _swap, address _feeTo, address _operator, uint _discount) public {
+    _setupRole(DEFAULT_ADMIN_ROLE, _admin);
+    discount = _discount;
+    swap = _swap;
+    feeTo = _feeTo;
+    mToken = _mToken;
+    operator = _operator;
+  }
+
+  function setDepedency(address _mToken, address _feeTo, uint _discount) external {
+    require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "not admin");
+    require(_discount <= 8, "invalid");
+    mToken = _mToken;
+    discount = _discount;
+    feeTo = _feeTo;
+  }
+
+  function tryPayToken(bytes32 key,address[] memory mTokenPath, uint256 _amountIn) internal returns(bool){
+    Order memory record =  ISwap(swap).records(key);
+
+    uint allowance = IEERC20(mToken).allowance(record.user, address(this));
+    if(allowance > 0) {
+      if(discount == 0) {
+        return true;
+      } else {
+        address router = ISwap(swap).router();
+        uint[] memory out = IIUniswapV2Router02(router).getAmountsOut(_amountIn.mul(997).mul(discount).div(1000).div(10), mTokenPath);
+        if(allowance >= out[out.length-1] && IEERC20(mToken).balanceOf(record.user) >= out[out.length-1]){
+          IEERC20(mToken).safeTransferFrom(record.user, feeTo, out[out.length-1]);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
+  function helpSwap(bytes32 key,address[] calldata mTokenPath, address[] calldata path, uint256 _amountIn, uint256 _amountOut, bool isReflect) external {
+    require(msg.sender == operator,"invalid sender");
+    require(mTokenPath[mTokenPath.length-1] == mToken,"invalid mtoken");
+    require(mTokenPath[0] == path[0], "invalid path");
+    bool isPaid = tryPayToken(key, mTokenPath, _amountIn);
+    ISwap(swap).swap(key, path, _amountIn, _amountOut, isReflect, isPaid);
+  }
+
+}
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,13 +1,37 @@
 const Swap = artifacts.require("Swap");
+const Helper = artifacts.require("Helper");
 const cfg=require('../script/config.js')
-module.exports = async function (deployer,network) {
+module.exports = async function (deployer,network, accounts) {
   console.log("ntwork:", network, cfg[network])
   if(network == 'development' || network == 'coverage') return
-  let admin = cfg[network].admin
-  let feeTo = cfg[network].feeTo
-  let operator = cfg[network].operator
-  let router = cfg[network].router
-  await deployer.deploy(Swap,admin,router,feeTo,operator);
+
+  let deployerAddr = accounts[0].toLowerCase();
+
+  let admin = cfg[network].admin.toLowerCase()
+  let feeTo = cfg[network].feeTo.toLowerCase()
+  let operator = cfg[network].operator.toLowerCase()
+  let router = cfg[network].router.toLowerCase()
+  let mToken = cfg[network].mToken.toLowerCase()
+  let discount  = 0
+  await deployer.deploy(Swap,deployerAddr,router,feeTo,operator);
   let sw = await Swap.deployed()
   console.log("sw address:", sw.address)
+
+  await deployer.deploy(Helper,admin,mToken, sw.address,feeTo,operator, discount);
+  let hp = await Helper.deployed()
+  console.log("hp address:", hp.address)
+
+
+
+
+  // set swap operator to the helper contractor.
+  await sw.setDepedency(feeTo, hp.address)
+
+  // set the real admin.
+  if (deployerAddr !== admin) {
+    console.log('renounceRole:', deployerAddr);
+    await airDrop.grantRole('0x00', cfg[network].admin);
+    await airDrop.renounceRole('0x00', deployerAddr);
+  }
+
 };
